9dcd988dd126967ea28435deff8e0a9c1a64cfb3
Bears <evanbero1980@gmail.com>
2021-02-04 23:43:35 -0600

    added embed and rendered live stream successfully

--- client/src/components/NewsComp/index.js
+++ client/src/components/NewsComp/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function NewsComp() {
     return (
         <div>
-
+            <Container>
+                <Row>
+                    <Col>
+                    Church News:
+                    Donations, time change, 
+                    food drive, serving opportunities
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 };
