ec5b5909052306485bd242a68ad8520fc1450dbc
Bears <evanbero1980@gmail.com>
2021-05-17 14:14:59 -0500

    styled about and staff page, added info about Pastor Joel on about page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -11,10 +11,14 @@ const About = () => {
           <Header id='header' />
         </Row>
         <Row className='justify-content-md-center'>
-          <h6 id='vision'>Mission statement:</h6>
+          <Col md='auto'>
+            <h6 id='about-h6' className='text-center'>
+              Mission statement:
+            </h6>
+          </Col>
         </Row>
-        <Row className='justify-content-md-center'>
-          <p>
+        <Row>
+          <p id='para' className='text-left'>
             At Redeemer Church, we seek to bring together the God's mission and
             the mission of the Church by making disciples who delight in,
             display, and declare the glorious gospel of Jesus Christ!
@@ -22,12 +26,17 @@ const About = () => {
         </Row>
 
         <Row className='justify-content-md-center'>
-          <h6 id='vision'>
-            Our Vision: Treasuring Christ and Proclaiming His Worth
-          </h6>
+          <Col md='auto'>
+            <h6 id='about-h6' className='text-center'>
+              Our Vision:
+            </h6>
+            <h6 id='about-h6' className='text-center'>
+              Treasuring Christ and Proclaiming His Worth
+            </h6>
+          </Col>
         </Row>
         <Row>
-          <p>
+          <p id='para' className='text-left'>
             Because God the Father is so delighted in His Son, He designed a
             masterful plan that stretches across all eternity to honor and exalt
             Jesus as the centerpiece of all heavenly and earthly existence. By
@@ -42,11 +51,66 @@ const About = () => {
           </p>
         </Row>
         <Row className='justify-content-md-center'>
-          <h6 id='about-joel'>About Pastor Joel Gilbert</h6>
+          <Col md='auto'>
+            <h6 id='about-h6' className='text-center'>
+              About Pastor Joel Gilbert
+            </h6>
+          </Col>
+        </Row>
+        <Row>
+          <p id='para' className='text-left'>
+            By His grace, God called me to preach His Word about 15 years ago.
+            He put His Word in my mouth and has not let me be silent since He
+            called me. At the end 2010 God called me to preach at Redeemer
+            Church. My aim in preaching is to exalt Christ, edify and strengthen
+            God’s people, and humble the sinner! My aim is preaching that uses
+            the truth of Scripture to shine the glory of God into the depths of
+            the soul to call people to live devotedly and totally for God.
+            Perhaps to sum up true biblical preaching is: it breaks us and
+            remakes us. With all my heart I desire such preaching that brings us
+            face to face with the most glorious and delightful Being in the
+            universe, and also face to face with our own profound wickedness. By
+            such preaching, the holy God binds Himself to sinful men — heart to
+            heart with a word of blood-bought grace.
+          </p>
         </Row>
         <Row className='justify-content-md-center'>
-          <p></p>
+          <Col md='auto'>
+            <p id='verse' className='text-left'>
+              “But speaking the truth in love, let us grow in every way into him
+              who is the head—Christ.” ||Ephesians 4:15||
+              <br />
+              “Now the goal of our instruction is love that comes from a pure
+              heart, a good conscience, and a sincere faith.” ||1 Timothy 1:5 ||
+            </p>
+          </Col>
         </Row>
+        <Row>
+          <p id='para' className='text-left'>
+            In preaching it’s not just that preachers need both truth and love.
+            Gospel truth has not reached its goal until it produces love. Love
+            has no living roots without gospel truth. Therefore, the goal and
+            aim of my preaching is that the truth of Christ would and must be
+            brought home to the heart by the Holy Spirit in order to produce
+            love. The aim in my preaching is NOT preaching that fills the head
+            but not the heart. I absolutely seek to stay away from trying to
+            inform and educate people apart from them being MOVED BY God’s glory
+            TO DO God’s will! The type of preaching that aims at puffing people
+            up with knowledge is at best, light without heat.
+          </p>
+
+          <p id='para' className='text-left'>
+            As a preacher I sincerely want my heart burn with love each week. If
+            the Spirit intends to work in the hearers of the Word, I’ve learned
+            that He generally works first in the preacher of the Word. In wisdom
+            I wholeheartedly covet the prayers of God’s people for the filling
+            of the Holy Spirit that I may preach in the power of the Spirit and
+            for the glory of God alone!
+            <br />
+            (Acts 4:8, 29–33; Eph. 6:18–20)
+          </p>
+        </Row>
+        <div id='bottom' />
       </Container>
     </Fragment>
   );
--- client/src/components/Giving/Giving.js
+++ client/src/components/Giving/Giving.js
@@ -7,7 +7,34 @@ import Header from '../Header';
 const Giving = () => {
   return (
     <Fragment>
-      <Container></Container>
+      <Container>
+        <Row></Row>
+        <Row>
+          <form
+            action='https://www.paypal.com/donate'
+            method='post'
+            target='_top'
+          >
+            <input type='hidden' name='business' value='8LQHMK6UJS5BY' />
+            <input type='hidden' name='currency_code' value='USD' />
+            <input
+              type='image'
+              src='https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif'
+              border='0'
+              name='submit'
+              title='PayPal - The safer, easier way to pay online!'
+              alt='Donate with PayPal button'
+            />
+            <img
+              alt=''
+              border='0'
+              src='https://www.paypal.com/en_US/i/scr/pixel.gif'
+              width='1'
+              height='1'
+            />
+          </form>
+        </Row>
+      </Container>
     </Fragment>
   );
 };
