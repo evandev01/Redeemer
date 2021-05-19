1206278f18bb7288b5a110df8b2134a746142e70
Bears <evanbero1980@gmail.com>
2021-05-19 18:23:35 -0500

    commit

--- src/components/Giving/Giving.js
+++ src/components/Giving/Giving.js
@@ -9,30 +9,8 @@ const Giving = () => {
     <Fragment>
       <Container>
         <Row></Row>
-        <Row>
-          <form
-            action='https://www.paypal.com/donate'
-            method='post'
-            target='_top'
-          >
-            <input type='hidden' name='business' value='8LQHMK6UJS5BY' />
-            <input type='hidden' name='currency_code' value='USD' />
-            <input
-              type='image'
-              src='https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif'
-              border='0'
-              name='submit'
-              title='PayPal - The safer, easier way to pay online!'
-              alt='Donate with PayPal button'
-            />
-            <img
-              alt=''
-              border='0'
-              src='https://www.paypal.com/en_US/i/scr/pixel.gif'
-              width='1'
-              height='1'
-            />
-          </form>
+        <Row className='justify-content-md-center'>
+          <h6 id='online-giving'>Online Giving Coming Soon!</h6>
         </Row>
       </Container>
     </Fragment>
