2bcda0cfe3af4de3bc6b0eac83fdedd5acb7f769
Bears <evanbero1980@gmail.com>
2021-05-20 17:19:45 -0500

    made youtube player responsive with consistent 16:9 aspect ratio

--- src/components/WatchLive/WatchLive.js
+++ src/components/WatchLive/WatchLive.js
@@ -10,19 +10,17 @@ const WatchLive = () => {
 
   return (
     <Fragment>
-      <Container id='stream'>
-        <Row className='justify-content-md-center'>
-          <iframe
-            id='player'
-            // width='560'
-            // height='315'
-            src='https://www.youtube.com/embed/5HUAL36GI-M'
-            title='YouTube video player'
-            frameborder='0'
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-            allowfullscreen
-          ></iframe>
-        </Row>
+      <Container id='stream' className='justify-content-md-center'>
+        <iframe
+          id='player'
+          // width='2000'
+          // height='1125'
+          src='https://www.youtube.com/embed/5HUAL36GI-M'
+          title='YouTube video player'
+          frameborder='0'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowfullscreen
+        ></iframe>
       </Container>
     </Fragment>
   );
