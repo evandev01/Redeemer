de9b00270ddf3621d10a4167e65fdb9f6e1b200b
Bears <evanbero1980@gmail.com>
2021-05-24 14:27:09 -0500

    centered player

--- src/components/WatchLive/WatchLive.js
+++ src/components/WatchLive/WatchLive.js
@@ -10,17 +10,19 @@ const WatchLive = () => {
 
   return (
     <Fragment>
-      <Container id='stream' className='justify-content-md-center'>
-        <iframe
-          id='player'
-          // width='2000'
-          // height='1125'
-          src='https://www.youtube.com/embed/5HUAL36GI-M'
-          title='YouTube video player'
-          frameborder='0'
-          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-          allowfullscreen
-        ></iframe>
+      <Container id='stream'>
+        <Row className='justify-content-md-center'>
+          <iframe
+            id='player'
+            // width='2000'
+            // height='1125'
+            src='https://www.youtube.com/embed/5HUAL36GI-M'
+            title='YouTube video player'
+            frameborder='1'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+            allowfullscreen
+          ></iframe>
+        </Row>
       </Container>
     </Fragment>
   );
