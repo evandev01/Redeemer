6de06695d2fa719553b6fa5d2f6e110769c600e6
Bears <evanbero1980@gmail.com>
2021-06-14 09:55:58 -0500

    s1

--- src/components/WatchLive/WatchLive.js
+++ src/components/WatchLive/WatchLive.js
@@ -13,7 +13,7 @@ const WatchLive = () => {
               id='player'
               width='560'
               height='315'
-              src='https://www.youtube.com/embed/4r0J42t1U6E'
+              src='https://www.youtube.com/embed/3rLIDuHM1Jk'
               title='YouTube video player'
               frameborder='0'
               allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
