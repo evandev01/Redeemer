525ce64d2a58ade5b3f5e0ad455069a44beb7000
Bears <evanbero1980@gmail.com>
2021-06-14 10:22:57 -0500

    testing autoplay feature

--- src/components/WatchLive/WatchLive.js
+++ src/components/WatchLive/WatchLive.js
@@ -13,16 +13,6 @@ const WatchLive = () => {
               id='player'
               width='560'
               height='315'
-              src='https://www.youtube.com/embed/3rLIDuHM1Jk'
-              title='YouTube video player'
-              frameborder='0'
-              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-              allowfullscreen='true'
-            ></iframe>
-            <iframe
-              id='player'
-              width='560'
-              height='315'
               src='https://www.youtube.com/embed/eH_sPWYzY8E'
               title='YouTube video player'
               frameborder='0'
@@ -44,7 +34,7 @@ const WatchLive = () => {
               src='https://www.youtube.com/embed/bNIifn8zUqc'
               title='YouTube video player'
               frameborder='0'
-              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+              allow='accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
               allowfullscreen='true'
             ></iframe>
           </Col>
