caae4cdbaf5b8bf34fea4972caa4aba314763ceb
Bears <evanbero1980@gmail.com>
2021-07-04 14:03:47 -0500

    scheduled live stream

--- src/components/WatchLive/WatchLive.js
+++ src/components/WatchLive/WatchLive.js
@@ -6,7 +6,24 @@ const WatchLive = () => {
   return (
     <Fragment>
       <Container id='stream-container'>
-        <Row id='stream-player' className='justify-content-center'>
+        {/* LIVE STREAM ========== */}
+        <Row id='stream-player' className='justify-content-center mt-5'>
+          <Col md='auto' />
+          <Col xs={12} sm={12} md={10} lg={10} className='text-center'>
+            <iframe
+              width='560'
+              height='315'
+              src='https://www.youtube.com/embed/79IX47QBdd8'
+              title='YouTube video player'
+              frameborder='0'
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+              allowfullscreen='true'
+            ></iframe>
+          </Col>
+        </Row>
+
+        {/* THIS WEEKS SERVICE ========== */}
+        <Row id='stream-player' className='justify-content-center pb-5'>
           <Col md='auto' />
           <Col xs={12} sm={12} md={10} lg={10} className='text-center'>
             <iframe
@@ -20,10 +37,9 @@ const WatchLive = () => {
               allowfullscreen='true'
             ></iframe>
           </Col>
-
-          <Col md='auto' />
         </Row>
 
+        {/* PROMO VIDEO ========== */}
         {/* <Row id='video-player' className='justify-content-center'>
           <Col md='auto' />
           <Col xs={12} sm={12} md={10} lg={10} className='text-center'>
